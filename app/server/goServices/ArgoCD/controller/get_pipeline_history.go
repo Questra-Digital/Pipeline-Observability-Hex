ada78fedb76340068ff9d5abb014486233ef25ca
madilkhan002 <ranaadil571@gmail.com>
2023-12-06 16:05:45 +0500

    refoctor pipeline_history and get_all_pipelines.go

--- app/server/goServices/ArgoCD/controller/get_pipeline_history.go
+++ app/server/goServices/ArgoCD/controller/get_pipeline_history.go
@@ -2,22 +2,77 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"log"
+
 	mongoconnection "github.com/QuestraDigital/goServices/ArgoCD/mongoConnection"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"github.com/joho/godotenv"
 )
 
+// IsPipelineAvailable checks if the requested pipeline is available
+func IsPipelineAvailable(pipelineName string, c *gin.Context) bool {
+	// Get all available pipelines
+	availblePipelines, err := GetAllPipelineNames()
+	if err != nil {
+		fmt.Println("Token error")
+		return false
+	}
+
+	// Check if the requested pipeline is in the list
+	_isPipelineAvailable := false
+	for _, availablePipeline := range availblePipelines {
+		if availablePipeline == pipelineName {
+			_isPipelineAvailable = true
+			break
+		}
+	}
+	return _isPipelineAvailable
+}
+
+// FindPipelineHistory retrieves pipeline history documents from MongoDB
+func findPipelineHistory(email, pipelineName string) ([]map[string]interface{}, error) {
+	client, err := mongoconnection.ConnectToMongoDB()
+	if err != nil {
+		return nil, err
+	}
+	defer client.Disconnect(context.TODO())
+
+	collection := client.Database("admin").Collection("argocd")
+
+	filter := bson.M{"email": email, "pipeline_name": pipelineName}
+	options := options.Find().SetSort(bson.D{{Key: "time", Value: -1}})
+
+	cursor, err := collection.Find(context.TODO(), filter, options)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.TODO())
+
+	var documents []map[string]interface{}
+	for cursor.Next(context.TODO()) {
+		var document map[string]interface{}
+		err := cursor.Decode(&document)
+		if err != nil {
+			fmt.Println("Error During Finding Document")
+			return nil, err
+		}
+		documents = append(documents, document)
+	}
+
+	return documents, nil
+}
+
 func PipelineHistory(c *gin.Context) {
-	
+
 	err := godotenv.Load(".env")
 	if err != nil {
-	  log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file")
 	}
 
 	// Get the pipeline parameter from the query string
@@ -29,22 +84,7 @@ func PipelineHistory(c *gin.Context) {
 		return
 	}
 
-	// Get all available pipelines
-	availblePipelines, err := GetAllPipelineNames()
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"message": "Token error"})
-		return
-	}
-
-	// Check if the requested pipeline is in the list
-	isPipelineAvailable := false
-	for _, availablePipeline := range availblePipelines {
-		if availablePipeline == pipelineName {
-			isPipelineAvailable = true
-			break
-		}
-	}
-
+	isPipelineAvailable := IsPipelineAvailable(pipelineName, c)
 	if !isPipelineAvailable {
 		c.JSON(http.StatusNoContent, gin.H{"message": "Requested pipeline is not available"})
 		return
@@ -52,44 +92,11 @@ func PipelineHistory(c *gin.Context) {
 
 	// Get the email from the authentication or wherever you have it
 	email := os.Getenv("USER_EMAIL")
-
-	// MongoDB connection
-	mongoClient, err := mongoconnection.ConnectToMongoDB()
+	history, err := findPipelineHistory(email, pipelineName)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error connecting to MongoDB"})
-		return
-	}
-	defer mongoClient.Disconnect(context.TODO())
-
-	// Get a handle to the database and collection
-	collection := mongoClient.Database("admin").Collection("argocd")
-
-	// Filter to find all documents for the given pipeline and email
-	filter := bson.M{"email": email, "pipeline_name": pipelineName}
-
-	// Options to sort by time in descending order
-	options := options.Find().SetSort(bson.D{{Key: "time", Value: -1}})
-
-	// Find all documents
-	cursor, err := collection.Find(context.TODO(), filter, options)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error finding documents"})
-		return
-	}
-	defer cursor.Close(context.TODO())
-
-	// Decode the cursor into an array of maps
-	var documents []map[string]interface{}
-	for cursor.Next(context.TODO()) {
-		var document map[string]interface{}
-		err := cursor.Decode(&document)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error decoding document"})
-			return
-		}
-		documents = append(documents, document)
 	}
 
 	// Send the array of documents as a JSON response
-	c.JSON(http.StatusOK, documents)
+	c.JSON(http.StatusOK, history)
 }
