e1fb609e70c0f68c25c15b39f5ab0df7a9090417
madilkhan002 <ranaadil571@gmail.com>
2023-12-08 23:25:32 +0500

    add separate service to run cronjob to monitor all pipelines

---
+++ app/server/goServices/ArgoCD-Monitor-Cronjob/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/QuestraDigital/goServices/ArgoCD-Monitor-Cronjob/controller"
+	"github.com/robfig/cron"
+)
+
+func runAllPipelinesStatusCronJob() {
+	// Create a new cron scheduler
+	c := cron.New()
+
+	// Define your cron job
+	job := cron.FuncJob(controller.AllPipelinesStatus)
+
+	// Add the cron job to the scheduler
+	c.AddJob("*/5 * * * * *", job) // This cron syntax means every 5 seconds
+
+	// Start the scheduler
+	c.Start()
+}
+
+func main() {
+	runAllPipelinesStatusCronJob()
+	select {}
+}
--- app/server/goServices/ArgoCD/controller/all_pipelines_status.go
+++ app/server/goServices/ArgoCD/controller/all_pipelines_status.go
@@ -1,11 +1,20 @@
 package controller
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-var counter map[string]int
+var (
+	counter     map[string]int
+	counterLock sync.Mutex
+)
 
 // update the counter and send notification to slack
 func updateCounter(isPipelineHealthy bool, pipeline_name string) {
+	counterLock.Lock()
+	defer counterLock.Unlock()
+
 	if !isPipelineHealthy {
 		counter[pipeline_name]++
 		if counter[pipeline_name] == 2 {
@@ -14,6 +23,34 @@ func updateCounter(isPipelineHealthy bool, pipeline_name string) {
 	} else {
 		counter[pipeline_name] = 0
 	}
+}
+
+func processPipeline(pipeline_name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	responseData, err := FetchPipelineData(pipeline_name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	summary := ParsePipelineData(responseData["nodes"].([]interface{}))
+
+	// checking health of all components of pipeline
+	isPipelineHealthy := true
+	checkHealth := func(component, status string) {
+		if status != "Healthy" {
+			fmt.Printf("%s : %s is not healthy\n", pipeline_name, component)
+			isPipelineHealthy = false
+		}
+	}
+
+	checkHealth("Pod", summary.Pod)
+	checkHealth("Deployment", summary.Deployment)
+	checkHealth("Service", summary.Service)
+	checkHealth("ReplicaSet", summary.ReplicaSet)
+
+	updateCounter(isPipelineHealthy, pipeline_name)
+	fmt.Println(counter)
 }
 
 func AllPipelinesStatus() {
@@ -25,28 +62,15 @@ func AllPipelinesStatus() {
 	allpipelines, err := GetAllPipelineNames()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	for _, pipeline_name := range allpipelines {
-		responseData, err := FetchPipelineData(pipeline_name)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		summary := ParsePipelineData(responseData["nodes"].([]interface{}))
-		// checking health of all components of pipeline
-		isPipelineHealthy := true
-		checkHealth := func(component, status string) {
-			if status != "Healthy" {
-				fmt.Printf("%s : %s is not healthy\n", pipeline_name, component)
-				isPipelineHealthy = false
-			}
-		}
 
-		checkHealth("Pod", summary.Pod)
-		checkHealth("Deployment", summary.Deployment)
-		checkHealth("Service", summary.Service)
-		checkHealth("ReplicaSet", summary.ReplicaSet)
-		updateCounter(isPipelineHealthy, pipeline_name)
-		fmt.Println(counter)
+	var wg sync.WaitGroup
+
+	for _, pipeline_name := range allpipelines {
+		wg.Add(1)
+		go processPipeline(pipeline_name, &wg)
 	}
+
+	wg.Wait()
 }
