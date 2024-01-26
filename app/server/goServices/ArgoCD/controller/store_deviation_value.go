32a56d3ef6e2e0d9905b98892cfa175f629ab70c
madilkhan002 <ranaadil571@gmail.com>
2024-01-27 00:24:10 +0500

    added api to store deviation value in MongoDB

--- app/server/goServices/ArgoCD/controller/store_deviation_value.go
+++ app/server/goServices/ArgoCD/controller/store_deviation_value.go
@@ -7,27 +7,13 @@ import (
 
 	mongoconnection "github.com/QuestraDigital/goServices/ArgoCD/mongoConnection"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Deviation struct {
 	Value string `json:"value"`
 }
 
-var collection *mongo.Collection
-
-func init() {
-	client, err := mongoconnection.ConnectToMongoDB()
-	if err != nil {
-		fmt.Println("Error while connecting to MongoDB: %v", err)
-		return
-	}
-	collection = client.Database("admin").Collection("deviations")
-}
-
-// func GetDeviationValue(key string) {
+// func GetDeviationValue(collection *mongo.Collection, key string) {
 // 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 // 	filter := bson.M{key: bson.M{"$exists": true}}
 // 	var result bson.M
@@ -40,31 +26,31 @@ func init() {
 // }
 
 func StoreDeviationValue(c *gin.Context, deviation_value string) {
-	// Parse JSON request body
-	deviation := Deviation{Value: deviation_value}
-
-	if err := c.ShouldBindJSON(&deviation); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	mongoClient, err := mongoconnection.ConnectToMongoDB()
+	if err != nil {
+		fmt.Println("Error: ", err)
 		return
 	}
+	defer mongoClient.Disconnect(context.TODO())
 
-	// Define a filter based on the Name field
-	filter := bson.M{"value": deviation.Value}
+	collection := mongoClient.Database("admin").Collection("deviations")
 
-	// Define an update document
-	update := bson.M{"$set": bson.M{"value": deviation.Value}}
-
-	// Set the upsert option to true
-	upsert := true
-
-	// Perform the upsert operation
-	result, err := collection.UpdateOne(context.TODO(), filter, update, &options.UpdateOptions{
-		Upsert: &upsert,
-	})
+	// Drop the collection
+	err = collection.Drop(context.TODO())
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Document upserted", "matchedCount": result.MatchedCount, "modifiedCount": result.ModifiedCount, "upsertedID": result.UpsertedID})
+	// Use deviation_value directly
+	deviation := Deviation{Value: deviation_value}
+
+	// Insert the new value
+	insertResult, err := collection.InsertOne(context.TODO(), deviation)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Deviation Value inserted....", "insertedID": insertResult.InsertedID})
 }
