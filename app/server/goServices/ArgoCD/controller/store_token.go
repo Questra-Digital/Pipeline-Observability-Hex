581a7d1c05d61104a4d7922791adc6c2d111b07f
madilkhan002 <ranaadil571@gmail.com>
2024-01-27 22:06:13 +0500

    store email and token in mongodb

--- app/server/goServices/ArgoCD/controller/store_token.go
+++ app/server/goServices/ArgoCD/controller/store_token.go
@@ -2,13 +2,48 @@
 package controller
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
+	mongoconnection "github.com/QuestraDigital/goServices/ArgoCD/mongoConnection"
 	"github.com/gin-gonic/gin"
 )
+
+type Token struct {
+	Value string `json:"Value"`
+}
+
+func StoreTokenInMongoDB(c *gin.Context, token string) {
+	mongoClient, err := mongoconnection.ConnectToMongoDB()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer mongoClient.Disconnect(context.TODO())
+
+	collection := mongoClient.Database("admin").Collection("argocdTokens")
+
+	// Drop the collection
+	err = collection.Drop(context.TODO())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create a new Token instance
+	tokenDocument := Token{Value: token}
+	fmt.Println("tokenDocument : ", tokenDocument)
+
+	// Insert the new Token instance into the database
+	_, err = collection.InsertOne(context.TODO(), tokenDocument)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+}
 
 func updateDotenv(key, value string) error {
 	// Read the content of the dotenv file
@@ -64,6 +99,8 @@ func StoreToken(c *gin.Context, token string) {
 		fmt.Println("Error:", err)
 		return
 	}
+	// store the token in the database
+	StoreTokenInMongoDB(c, token)
 
 	fmt.Println("Token Saved successfully")
 	c.JSON(http.StatusOK, gin.H{"message": "Token Saved successfully"})
