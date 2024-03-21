0e51503e31ce3b1233da6d708aa1fe4714a8cf1d
sheerazmehboob <sherazmehboob14@gmail.com>
2024-03-22 01:04:24 +0500

    Fixed Pipeline Row

--- app/client/src/components/organisms/Dashboard/Pipelines.js
+++ app/client/src/components/organisms/Dashboard/Pipelines.js
@@ -1,9 +1,33 @@
 "use client";
-import { pieplines } from "@/constants/pipelines";
+import React, { useState, useEffect } from "react";
+import instance from "@/axios/axios";
 import { useRouter } from "next/navigation";
+import { ErrorToast } from "@/components/atoms/toastUtils/Toast";
 
 const Pipelines = () => {
+  const [pipelines, setPipelines] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchPipelines() {
+      try {
+        const response = await instance.get("/all_pipelines", {});
+        setPipelines(response.data.available_pipeline);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching files:", error);
+        if (error.response.status == 401) {
+          ErrorToast('Please Configure ArgoCD First!');
+        }
+      }
+      finally{
+        setIsLoading(false);
+      }
+    }
+    fetchPipelines();
+  }, []);
 
   const handleRowClick = (pipelineName) => {
     router.push(
@@ -12,58 +36,45 @@ const Pipelines = () => {
   };
 
   return (
-    <div className="h-full w-full p-2 xs:p-5 md:p-10 overflow-x-auto">
-      <table className="w-full text-sm text-left text-gray-400 table-auto shadow-md shadow-purple-900">
-        <thead className="uppercase bg-gray-700">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              pipeline name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Executions
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Failures
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Median
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Last Build
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {pieplines.map((pipeline, index) => (
-            <tr
-              key={index}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-600"
-              onClick={() => handleRowClick(pipeline.name)}
-            >
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {pipeline.name}
+    <div className="h-full w-full md:w-[80%] p-2 xs:p-5 md:p-10 overflow-x-auto">
+      {isLoading && <p className="text-center">Loading Your Pipelines...</p>}
+      {!isLoading && (
+        <table className="w-full text-sm text-left text-gray-400 table-auto shadow-md shadow-purple-900">
+          <thead className="uppercase bg-gray-700">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                pipeline name
               </th>
-              <td className="px-6 py-4">{pipeline.executions}</td>
-              <td className="px-6 py-4">{pipeline.failures}</td>
-              <td className="px-6 py-4">{pipeline.median}</td>
-              <td className="px-6 py-4">
-                <span
-                  className={`px-2 py-1 rounded-md ${
-                    pipeline.last_build === "Failed"
-                      ? "bg-red-600 text-white"
-                      : "text-white bg-green-600"
-                  }`}
-                >
-                  {pipeline.last_build}
-                </span>
-              </td>
+              <th scope="col" className="px-6 py-3">
+                Actions
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {pipelines.map((pipeline, index) => (
+              <tr
+                key={index}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-600"
+                onClick={() => handleRowClick(pipeline)}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {pipeline}
+                </th>
+                <td className="px-6 py-4">
+                  <span
+                    className={`px-5 py-3 rounded-md text-white bg-green-600`}
+                  >
+                    Dashboard
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
