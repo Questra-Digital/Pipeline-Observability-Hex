f70c1ae2f3e358b4d68902c022073876c2a474fc
madilkhan002 <ranaadil571@gmail.com>
2024-05-12 16:59:17 +0500

    store notifier email in mongodb

---
+++ app/server/ArgoCD/ArgoCD-Web-App/controller/notifier_email.go
@@ -0,0 +1,49 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	mongoconnection "github.com/QuestraDigital/goServices/ArgoCD-Web-App/mongoConnection"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func StoreNotifierEmail(c *gin.Context) {
+	var emailCredentials map[string]string
+	if err := c.BindJSON(&emailCredentials); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+	email := emailCredentials["email"]
+	password := emailCredentials["password"]
+
+	fmt.Println("Email: ", email)
+	fmt.Println("Password: ", password)
+	// connect to MongoDB
+	mongoClient, err := mongoconnection.ConnectToMongoDB()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer mongoClient.Disconnect(context.Background())
+	collection := mongoClient.Database("notification").Collection("email_notifier")
+	// delete existing email and password
+	_, err = collection.DeleteMany(context.Background(), bson.D{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// store email and password in MongoDB
+	_, err = collection.InsertOne(context.Background(), bson.D{
+		{Key: "email", Value: email},
+		{Key: "password", Value: password},
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Email stored successfully"})
+}
